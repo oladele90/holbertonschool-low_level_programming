fbe339528871a3075836a9f6d9c2c9ea3e0e2cb4
oladele90 <oladelemiro07@gmail.com>
2022-09-15 11:59:20 -0700

    betty sucks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/*
+ *File: 4-puts.c
+ *Author: Oladele M.
+ */
+#include <stdio.h>
+/**
+ *main - Prints "Programming is like building a multilingual puzzle"
+ *Return: Always 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle")
+return (0);
+}
