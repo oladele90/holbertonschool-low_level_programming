4997f84e90d45acb538938d4a6aa3223bd4ddf08
oladele90 <oladelemiro07@gmail.com>
2022-09-19 10:10:12 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+/**
+*main - negative or positive
+*Return: always 0
+*/
+#include <stdio.h>
+int main(void)
+{
+int a;
+for (a = '0'; a <= '9'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
