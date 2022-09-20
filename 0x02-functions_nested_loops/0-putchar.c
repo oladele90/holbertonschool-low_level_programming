481341198c741aa0d2100c2fa940cde25f968caa
oladele90 <oladelemiro07@gmail.com>
2022-09-20 09:27:54 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
 */
 #include "main.h"
 
-int main(void)
+int _putchar(char c)
 {
   _putchar('_');
   _putchar('p');
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,11 @@
-int _putchar(int)
+#ifndef HEAD_H
+#define HEAD_H
+#include <unistd.h>
+/**
+ *_putchar - writes the character c to stdout
+ *Return: on success 1 and on error -1
+ */
+
+int _putchar(char c)
+#endif
+  
