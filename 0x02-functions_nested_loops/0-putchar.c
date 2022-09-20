69b3566ec3a4f1ca6d1fa2fac1833abf2f58128a
oladele90 <oladelemiro07@gmail.com>
2022-09-20 09:14:31 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,8 +3,17 @@
 *Return: 0
 */
 #include "main.h"
+
 int main(void)
 {
-printput;
-return (0);
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
+  return (0);
 }
