c6925902ba93d1d4b3e2556a8b8e96932c7468d2
oladele90 <oladelemiro07@gmail.com>
2022-09-20 10:05:34 -0700

    putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
  * Return: Always 0.
  */
 void print_alphabet(void)
 {
 char i;
-for (i='a'; i<='z'; i++)
+for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
 }
