4efca4333777e3a4dd48a7f0784ebe13ec73283b
oladele90 <oladelemiro07@gmail.com>
2022-09-20 18:15:21 -0700

    pid

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* jack_bauer - print the minutes in a day.
+*Return:
+*/
+void jack_bauer(void)
+{
+int n = 0;
+int m = 0;
+ int o = 0;
+ int p = 0;
+ 
+ while (n <= 2 && m <= 3)
+   {
+     _putchar('0' + n);
+       _putchar('0' + m);
+       _putchar(':');
+       _putchar('0' + o);
+       _putchar('0' + p);
+       n++;
+       return;
+   }
+}
