e0f1a00d1dc36d3cbf008ca87d4a7428b22107c9
oladele90 <oladelemiro07@gmail.com>
2022-09-21 09:39:39 -0700

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,13 +11,31 @@ while (m <= 9)
 while (n < 9)
 {
 l = (m * n);
+ if (l <= 9)
+   {
 _putchar(l + '0');
 _putchar(',');
 _putchar(' ');
+   }
+ else
+   {
+_putchar(l);
+_putchar(',');
+_putchar(' ');
+   }
 n++;
 }
+ if (l <= 9)
+   {
 _putchar(l + '0');
 n = 0;
 m++;
+   }
+ else
+{
+_putchar(l + '0');
+n = 0;
+m++;
+}
 }
 }
