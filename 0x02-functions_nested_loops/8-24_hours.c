3444336bcf07e08bfb21cca829a6af9b16f9c0ca
oladele90 <oladelemiro07@gmail.com>
2022-09-20 20:37:15 -0700

    24 hrs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,33 +7,32 @@ void jack_bauer(void)
 {
 int n = 0;
 int m = 0;
- int o = 0;
- int p = 0;
- 
- while (n <= 2)
-   {
-     while ((m <= 9 && n <= 1) || (m <= 3 && n <= 2))
-       {
-	 while (o <= 5)
-	   {
-	     while (p <= 9)
-	       {
-       _putchar('0' + n);
-       _putchar('0' + m);
-       _putchar(':');
-       _putchar('0' + o);
-       _putchar('0' + p);
-       _putchar('\n');
-       p++;
-	       }
-	     o++;
-	     p = 0;
-	   }
-	 m++;
-	 o = 0;
-       }
-     n++;
-     m = 0;
-   }
- return;
+int o = 0;
+int p = 0;
+while (n <= 2)
+{
+while ((m <= 9 && n <= 1) || (m <= 3 && n <= 2))
+{
+while (o <= 5)
+{
+while (p <= 9)
+{
+_putchar('0' + n);
+_putchar('0' + m);
+_putchar(':');
+_putchar('0' + o);
+_putchar('0' + p);
+_putchar('\n');
+p++;
+}
+o++;
+p = 0;
+}
+m++;
+o = 0;
+}
+n++;
+m = 0;
+}
+return;
 }
