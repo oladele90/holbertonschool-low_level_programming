7184ed02d2ded69382fbda6178356da639503c16
oladele90 <oladelemiro07@gmail.com>
2022-09-22 12:25:22 -0700

    most-numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - print more numbers
+ *
+ * Return: nothing
+ */
+void print_most_numbers(void)
+{
+int i = 0;
+
+while (i <= 9)
+{
+if ((i != 2) && (i != 4))
+{
+_putchar (i + '0');
+}
+i++;
+}
+_putchar ('\n');
+}
