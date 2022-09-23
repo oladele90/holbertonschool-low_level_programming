6aaa1bd553c361bebe28dcac73d3f53d356776ef
oladele90 <oladelemiro07@gmail.com>
2022-09-23 10:28:46 -0700

    fizzy

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,25 +13,14 @@ int main(void)
 	while (i <= 100)
 	{
 	if (i % 15 <= 0)
-	{
 		printf("FizzBuzz");
-		putchar(' ');
-	}
 	else if (i % 5 <= 0)
-	{
 		printf("Buzz");
-		putchar(' ');
-	}
 	else if (i % 3 <= 0)
-	{
 		printf("Fizz");
-		putchar(' ');
-	}
 	else
-	{
 		printf("%d", i);
-		putchar(' ');
-	}
+	putchar(' ');
 	i++;
 	}
 	putchar('\b');
