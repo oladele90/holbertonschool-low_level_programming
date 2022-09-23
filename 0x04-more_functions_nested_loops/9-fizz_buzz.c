c5bdd38599d6e89c4927d2767c60f8b7aec71f13
oladele90 <oladelemiro07@gmail.com>
2022-09-23 10:02:15 -0700

    fizzy

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ *main - maka da buzz
+ *
+ *Return: o
+ *
+ */
+
+int main(void)
+{
+	int i = 1;
+	while (i <= 100)
+	{
+	if (i % 15 <= 0)
+		printf("FizzBuzz ");
+	else if (i % 5 <= 0)
+		printf("Buzz ");
+	else if (i % 3 <= 0)
+		printf("Fizz ");
+	else
+	{
+		printf("%d ", i);
+	}
+	i++;
+	}
+	putchar('\n');
+	return (0);
+}
