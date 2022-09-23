bc6062a39acc46263f002573112e8629a28c9bc7
oladele90 <oladelemiro07@gmail.com>
2022-09-23 08:29:55 -0700

    i_did_it

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_diagonal - make diagonal lines
+ *@n: number of times n will be printed
+ * Return: n values.
+ */
+
+void print_diagonal(int n)
+{
+	int m = 1, i = m;
+	while (n > 1)
+	{
+		_putchar('\\');
+		_putchar('\n');
+		if (n != 0)
+		{
+			while (i > 0)
+			{
+				_putchar(' ');
+				i--;
+			}
+			m++;
+			i = m;
+		}
+		n--;
+	}
+	if (n >= 1)
+	{
+		_putchar('\\');
+	}
+	_putchar('\n');
+}
