1b8f53a9e1dfe6ff7011c835bdb3bab589306e1b
oladele90 <oladelemiro07@gmail.com>
2022-09-26 12:32:53 -0700

    put_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -24,7 +24,7 @@ void puts_half(char *str)
 	}
 	if (count % 2 != 0)
 	{
-		for (i = ((count - 1) / 2); ((i  <= count) && (i != '\0')); i++)
+		for (i = ((count - 1) / 2); ((i  <= count) && (i != '\0')); ++i)
 		{
 			_putchar(str[i]);
 		}
