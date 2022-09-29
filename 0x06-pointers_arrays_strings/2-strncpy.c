a802f309a732757cc4665adb0c3dbc0d36cb6817
oladele90 <oladelemiro07@gmail.com>
2022-09-29 10:18:35 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,26 @@
+#include "holberton.h"
+
 /**
  * _strncpy - copy string
  *@dest: ponter to string that should be returned
  *@src: ponter to string that needs to be copied
+ *@n: amount of characters to copy
  * Return: Always 0.
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-        int  i = 0;
+int  i = 0;
 
-        while ((i < n) && (src[i] != '\0'))
-        {
-                dest[i] = src[i];
-		i++;
-        }
-	while (i < n)
-	{
+while ((i < n) && (src[i] != '\0'))
+{
+	dest[i] = src[i];
+	i++;
+}
+while (i < n)
+{
 	dest[i] = '\0';
 	i++;
-	}
-        return (dest);
+}
+return (dest);
 }
