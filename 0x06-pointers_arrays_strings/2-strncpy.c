52d7e1d03114f61f38c6a8c68524c66c0650b50a
oladele90 <oladelemiro07@gmail.com>
2022-09-29 10:14:48 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+/**
+ * _strncpy - copy string
+ *@dest: ponter to string that should be returned
+ *@src: ponter to string that needs to be copied
+ * Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+        int  i = 0;
+
+        while ((i < n) && (src[i] != '\0'))
+        {
+                dest[i] = src[i];
+		i++;
+        }
+	while (i < n)
+	{
+	dest[i] = '\0';
+	i++;
+	}
+        return (dest);
+}
