36c3420f20fdcb93347fde41446955841c5dc042
oladele90 <oladelemiro07@gmail.com>
2022-09-30 11:45:13 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - check for occurrence of character
+ * @s: pointer to string
+ * @c: character that function will check for
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	return ('\0');
+}
