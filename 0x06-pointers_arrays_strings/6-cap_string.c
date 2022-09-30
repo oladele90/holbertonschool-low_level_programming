58958aacb6f16acc3a16bd8b174520ef2a888a72
oladele90 <oladelemiro07@gmail.com>
2022-09-30 07:52:53 -0700

    cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * cap_string - change words from lower case to upper
+ *@str: value manipulated and  returned to function
+ * Return: new string
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	if (str[i] >= 'a' && str[i] <= 'z')
+
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z') && (str[i - 1] == ',' ||
+		   str[i - 1] == ';' || str[i - 1] == '.' ||
+		   str[i - 1] == '!' || str[i - 1] == '?' ||
+		   str[i - 1] == '"' || str[i - 1] == '(' ||
+		   str[i - 1] == ')' || str[i - 1] == '{' ||
+		   str[i - 1] == '}' || str[i - 1] == ' ' ||
+		   str[i - 1] == '\n' || str[i - 1] == '\t'))
+		{
+			str[i] = str[i] - ' ';
+		}
+		i++;
+	}
+	return (str);
+}
