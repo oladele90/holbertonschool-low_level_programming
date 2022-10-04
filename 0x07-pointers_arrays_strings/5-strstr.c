7495c1d21a9c57711e1ffa734d2eb3cf932cfe06
oladele90 <oladelemiro07@gmail.com>
2022-10-03 18:22:15 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,13 +12,13 @@ char *_strstr(char *haystack, char *needle)
 {
         int i = 0, c = 0, t;
 
-	if (haystack[i] == '\0')
-		return (NULL);
         while (haystack[i] != '\0')
         {
 		t = i;
 		while (needle[c] != '\0' && needle[c] == haystack[i])
 				    {
+					    if (haystack[i] == '\0')
+						    return (NULL);
 					    c++;
 					    i++;
 					    if (needle[c] == haystack[i] &&
