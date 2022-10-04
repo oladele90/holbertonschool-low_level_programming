0e4bf3e3527213ca26f4676af560aa7660a8fce7
oladele90 <oladelemiro07@gmail.com>
2022-10-03 17:57:37 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * _strstr - check for first occurence of characters from string
+ * @haystack: original string
+ * @needle: string of characters to check for
+ * Return: Always 0.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+        int i = 0, c = 0, t;
+
+        while (haystack[i] != '\0')
+        {
+		t = i;
+		while (needle[c] != '\0' && needle[c] == haystack[i])
+				    {
+					    c++;
+					    i++;
+					    if (needle[c] == haystack[i] &&
+						needle [c + 1] == '\0')
+					    {
+						    return (haystack + t);
+					    }
+				    }
+		c = 0;
+		while (needle[c] != '\0' && needle[c] != haystack[i])
+			i++;
+	}
+	return (NULL);
+}
