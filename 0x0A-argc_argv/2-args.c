cc0db4e21f2a7ee7a2a0b89a14e70584ff9f4dac
oladele90 <oladelemiro07@gmail.com>
2022-10-11 08:55:16 -0700

    args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: argument count void
+ * @argv: argument vector [0] returns program name
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
