a07566c459b39b577c09b343e8ba97b610ff9c66
oladele90 <oladelemiro07@gmail.com>
2022-10-11 07:46:11 -0700

    my name is

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,14 @@
 
 /**
  * main - prints its name
+ * @argc: argument count void
+ * @argv: argument vector [0] returns program name
+ * Return: 0
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf ("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
