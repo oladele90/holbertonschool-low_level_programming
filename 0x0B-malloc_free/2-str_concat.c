4b86bee965fc8cfca12ccd6d72ae5d4e15ec697e
oladele90 <oladelemiro07@gmail.com>
2022-10-13 07:08:35 -0700

    concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - makes new space in memory and copies a given strings into it
+ * @s1: pointer to string that needs to be copied
+ * @s2: pointer to string that needs to be copied
+ * Return: pointer to new string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+char *str;
+int i = 0, j = 0;
+
+if ((s1 == NULL) && (s2 == NULL))
+{
+	return (NULL);
+}
+while (s1[i] != '\0')
+{
+	i++;
+}
+while (s2[j] != '\0')
+{
+	j++;
+}
+j++;
+str = malloc(sizeof(*str) * (i + j));
+for (i = 0; s1[i] != '\0'; i++)
+{
+	str[i] = s1[i];
+}
+for (j = 0; s2[j] != '\0'; j++)
+{
+	str[i] = s2[j];
+	i++;
+}
+return (str);
+}
