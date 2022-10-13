a9e383c73d5a1f08c0ff6a4151f0087a4a6aeb69
oladele90 <oladelemiro07@gmail.com>
2022-10-13 09:21:23 -0700

    concat

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - print the sum of diagonals in matrix
+ * @a: points to spot in matrix
+ * @size: size of matrix
+ * Return: nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+	
