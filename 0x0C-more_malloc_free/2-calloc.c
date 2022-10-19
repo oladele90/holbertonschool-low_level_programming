a4896bbce2fa6258a13fb6c3519572a7da5e5ba1
oladele90 <oladelemiro07@gmail.com>
2022-10-19 12:13:43 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *array;
 	unsigned int n = 0;
 
-	if ((nmemb <= 0) || (size <= 0))
+	if ((nmemb == 0) || (size == 0))
                 return (NULL);
 	array = malloc(nmemb * size);
 
