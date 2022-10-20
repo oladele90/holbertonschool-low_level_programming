61f506a3b5cf1de22252fefa64d0f9361e270e88
oladele90 <oladelemiro07@gmail.com>
2022-10-20 10:18:48 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,7 +13,4 @@ struct dog
 	float age;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
 #endif
