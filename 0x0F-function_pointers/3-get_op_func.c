387d3b8279727f46cf47de4d6e3ed056ea9aae6d
oladele90 <oladelemiro07@gmail.com>
2022-10-24 14:33:55 -0700

    calc

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,16 +1,22 @@
 #include "3-calc.h"
 #include <stdlib.h>
 
+/**
+ * get_op_func - gets function to use in main code
+ * @s: operator
+ *Return: pointer to function
+ */
+
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 	int i = 0;
 
 	if (*(ops[i].op) != *s && ops[i].op != NULL)
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 /**
  * main - performs simple operations
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: 0 for success
  */
 
 int main(int __attribute__((__unused__)) argc, char *argv[])
@@ -13,7 +16,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
 	num1 =  atoi(argv[1]);
 	num2 = atoi(argv[3]);
@@ -21,13 +24,13 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	if (get_op_func(op) == NULL)
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
 	if ((*op == '/' && num2 == 0)
 	    || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
