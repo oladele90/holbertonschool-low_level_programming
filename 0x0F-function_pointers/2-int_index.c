193cafb0422d44dbc3325da9c9fe01504b209cf2
oladele90 <oladelemiro07@gmail.com>
2022-10-24 17:35:03 -0700

    index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,18 +5,19 @@
  * @array: pointer to array
  * @size: size of array
  * @cmp: pointer to function
+ * Return: pointer to integer that matches or -1 for error
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
-		    return (i);
+			return (i);
 	}
 	return (-1);
 }
