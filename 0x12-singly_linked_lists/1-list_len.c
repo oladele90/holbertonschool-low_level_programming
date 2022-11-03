97691af022e2ec4bb87ff88fc8ce6a2d541b22e8
oladele90 <oladelemiro07@gmail.com>
2022-11-03 10:17:20 -0700

    list len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * list_len - prints elements of all nodes in linked list
+ * @h: pointer to node in linked list
+ * Return: an unsigned integer thet represents total amount of nodes in list
+ */
+
+size_t list_len(const list_t *h)
+{
+        unsigned int count = 0;
+        const list_t *temp = h;
+
+        while (temp)
+        {
+		temp = temp->next;
+		count++;
+        }
+        return (count);
+}
