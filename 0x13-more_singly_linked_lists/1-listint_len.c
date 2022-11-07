7ef64346a63443cd96ae0ffdc5a218d3b930b400
oladele90 <oladelemiro07@gmail.com>
2022-11-07 10:02:29 -0800

    integer count

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - counts elements of a list
+ * @h: pointer to a list of elements
+ * Return: number of elements printed
+ */
+
+size_t listint_len(const listint_t *h)
+{
+        unsigned int count = 0;
+
+        if (h == NULL)
+                return (0);
+        while (h)
+        {
+                count++;
+                h = h->next;
+        }
+        return (count);
+}
