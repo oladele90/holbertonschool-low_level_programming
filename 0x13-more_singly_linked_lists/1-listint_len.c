d962b95e7f9768a7101fc67b32f83955104c1b4b
oladele90 <oladelemiro07@gmail.com>
2022-11-07 10:03:34 -0800

    integer count

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -11,14 +11,14 @@
 
 size_t listint_len(const listint_t *h)
 {
-        unsigned int count = 0;
+unsigned int count = 0;
 
-        if (h == NULL)
-                return (0);
-        while (h)
-        {
-                count++;
-                h = h->next;
-        }
-        return (count);
+if (h == NULL)
+	return (0);
+while (h)
+{
+	count++;
+	h = h->next;
+}
+return (count);
 }
