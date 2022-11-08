2c7c9ff05a236a3aeb53a78dc25242767ce94cac
oladele90 <oladelemiro07@gmail.com>
2022-11-07 17:40:53 -0800

    get node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the indexth node
+ * @head: pointer to head node
+ * @index: reference to nth node
+ * Return: pointer to nth node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *find_node;
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+	find_node = head;
+	for (i = 0; i < index; i++)
+		find_node = find_node->next;
+	return (find_node);
+}
