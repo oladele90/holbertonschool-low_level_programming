02c551cb587f12ff9a7b2948be287a7465b4d6b4
oladele90 <oladelemiro07@gmail.com>
2022-11-09 14:11:20 -0800

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	o = open(filename, O_WRONLY | O_APPEND);
 	if (o == -1)
 		return (-1);
-	w = write(o,text_content, len);
+	w = write(o, text_content, len);
 	if (w == -1)
 		return (-1);
 	close(0);
