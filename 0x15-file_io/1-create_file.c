f0757bb17ce692ab67f70714ac539004e058d80c
oladele90 <oladelemiro07@gmail.com>
2022-11-09 13:51:58 -0800

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: points to file name of file to create
+ * @text_content: pointer to string to write in a file
+ * Return 1 for success or -1 for fail
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for(len = 0; text_content[len];)
+			len++;
+	}
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (o == -1)
+		return (-1);
+	w = write(o, text_content, len);
+	if (w == -1)
+		return (-1);
+	close(o);
+	return (1);
+}
