f898e9706fe53aad1d90fbb13089d66b574ba84e
oladele90 <oladelemiro07@gmail.com>
2022-11-08 18:38:20 -0800

    read textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output.
+ *@filename: pointer to file to be read
+ *@letters: number of letters to be read and printed
+ * Return: Always 0.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buf;
+
+	buf = malloc(sizeof(char) * letters);
+	o = open(filename, 000);
+	r = read(o, buf, letters);
+	w = write(1, buf, r);
+	close(o);
+	return (w);
+}
