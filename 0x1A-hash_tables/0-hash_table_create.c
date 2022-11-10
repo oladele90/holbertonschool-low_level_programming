9f0bf3b039cbfdb2a679d9141ea0ca239cedfc20
oladele90 <oladelemiro07@gmail.com>
2022-11-10 09:25:56 -0800

    hash table create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,5 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	my_hash->size = size;
 	my_hash->array = malloc(sizeof(hash_node_t) * size);
+	if (my_hash->array == NULL)
+		return (NULL);
 	return (my_hash);
 }
