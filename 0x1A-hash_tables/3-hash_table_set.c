760b383e680fa8ce699b493910359a9595990c70
oladele90 <oladelemiro07@gmail.com>
2022-11-10 12:26:29 -0800

    hash set

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: pointer to hash table
+ * @key: key
+ * @value: value associated with key
+ * Return: 1 for success or 0 for fail
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned int hash;
+	hash_node_t *my_hash;
+
+	my_hash = malloc(sizeof(hash_node_t));
+	if (!my_hash)
+	{
+		free(my_hash);
+		return (0);
+	}
+	if (key == NULL)
+		return (0);
+	my_hash->key = strdup(key);
+	if (value == NULL)
+		my_hash->value = "";
+	else
+		my_hash->value = strdup(value);
+	hash = key_index((unsigned char*)key, ht->size);
+	ht->array[hash] = my_hash;
+	return (1);
+}
