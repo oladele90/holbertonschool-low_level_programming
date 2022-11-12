0e14b4142e31bad18a2a9327deec5cc813e35fb2
oladele90 <oladelemiro07@gmail.com>
2022-11-12 13:42:42 -0800

    hash get

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,7 +9,7 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned int i = key_index((unsigned char *)key, ht->size);
+	unsigned int i = key_index((const unsigned char *)key, ht->size);
 
 	if (i >= ht->size)
 		return (NULL);
