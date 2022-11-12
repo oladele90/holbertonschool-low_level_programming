aeb63b44edd3ef09fe95bb5595ebdf1b11ab8877
oladele90 <oladelemiro07@gmail.com>
2022-11-12 12:20:32 -0800

    ht

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,21 +12,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned int hash;
 	hash_node_t *my_hash;
-	char *value_copy;
 
+	if (ht == NULL)
+		return (0);
 	my_hash = malloc(sizeof(hash_node_t));
 	if (!my_hash)
 	{
 		free(my_hash);
 		return (0);
 	}
-	if (key == NULL || key[0] == '\0')
+	if (key == NULL || *key == '\0')
 		return (0);
 	my_hash->key = strdup(key);
 	if (value == NULL)
 		return (0);
-	value_copy = strdup(value);
-		my_hash->value = value_copy;
+	my_hash->value = strdup(value);
 	hash = key_index((unsigned char *)key, ht->size);
 	if (ht->array[hash] != NULL)
 	{
