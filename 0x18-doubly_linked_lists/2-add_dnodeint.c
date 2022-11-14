14777157a41cbd6532dc9ef3d5aba67caccb84b3
oladele90 <oladelemiro07@gmail.com>
2022-11-14 11:50:58 -0800

    add node at beginning

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	node->next = *head;
 	node->prev = NULL;
 	*head = node;
-	if (head == NULL)
+	if (*head == NULL)
 		return (NULL);
 	return (*head);
 }
