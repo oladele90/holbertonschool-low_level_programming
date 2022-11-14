80202c2269f54933c0548e7392690794f8c24007
oladele90 <oladelemiro07@gmail.com>
2022-11-14 11:39:54 -0800

    add node at beginning

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *node;
 
-	if (n = NULL)
+	if (n > 2147483647 || n < -2147483647)
 		return (NULL);
 	node = malloc(sizeof(dlistint_t));
 	if (!node)
