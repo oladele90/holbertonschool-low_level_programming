131d3098358d5a76dcef402045c0de0cf8929226
oladele90 <oladelemiro07@gmail.com>
2022-11-13 17:08:57 -0800

    hash print

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints hash table
+ * @ht: the hash table to be printed
+ * Return: nothing
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i, x;
+	hash_node_t *node;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i <= ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (x > 1)
+				printf(", ");
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			x++;
+		}
+	}
+	printf("}\n");
+}
