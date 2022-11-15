34e9e6d6c58faea46909cf0f87eb362d52def67f
oladele90 <oladelemiro07@gmail.com>
2022-11-15 09:40:51 -0800

    get nth node

--- 0x18-doubly_linked_lists/5-get_dnodeint.c
+++ 0x18-doubly_linked_lists/5-get_dnodeint.c
@@ -16,8 +16,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		return (NULL);
 	node = head;
 	for (i = 0; i < index; i++)
+	{
 		node = node->next;
-	if (node == NULL)
-		return (NULL);
+		if (node == NULL)
+			return (NULL);
+	}
 	return (node);
 }
