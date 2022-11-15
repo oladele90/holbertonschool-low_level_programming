51c3ede830e8d85b082a6bff2a0eb918c4e3e712
oladele90 <oladelemiro07@gmail.com>
2022-11-15 09:49:01 -0800

    sum

---
+++ 0x18-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of data in list
+ * @head: pointer to the head of a node
+ * Return: sum of data or 0
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+        dlistint_t *node;
+        int i = 0;
+
+        if (head == NULL)
+                return (0);
+        for (node = head; node != NULL; node = node->next)
+                i += node->n;
+        return (i);
+}
