6c10f893b6b5ef6b34937494e4f9abdeab613725
oladele90 <oladelemiro07@gmail.com>
2022-11-15 19:29:37 -0800

    delete

---
+++ 0x18-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes node at index
+ * @index: nodeto be deleted
+ * @head: points to pointer of head of list
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *node;
+
+	node = *head;
+}
